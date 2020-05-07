96405e42ff618d48f20a019589c7e3584b9e4975
Yerassil <yerassil@gmail.com>
2020-05-07 22:46:11 +0600

    Add admin views for models

---
+++ subaruhelp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import ObdCode, Comment
+
+
+@admin.register(ObdCode)
+class ObdCodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'obd_code', 'pub_date', 'car_model')
